e245f1a2004a61e82a527313d05c03930c55fce5
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-06 00:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
 let textTag=document.getElementById('evaluatedText');
 let headerTag=document.getElementById('letterCount');
-let textValue=textTag.value;
 
+let textValue=textTag.value;
 headerTag.innerHTML=textValue.length;
